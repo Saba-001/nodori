6830b0db6abc707568ba92ede9e67f0fe6dbf3c5
Saba Janelidze <s.janelidze@bitnet.ge>
2024-06-17 13:03:37 +0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  // eslint-disable-next-line no-undef
-  base: process.env.VITE_BASE_URL || "/",
+export default defineConfig(() => ({
+  base: "/nodori",
   plugins: [react()],
-});
+}));
