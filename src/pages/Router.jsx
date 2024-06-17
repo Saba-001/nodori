33ae2042df40997071406a88bfede617a396d1f7
Saba Janelidze <s.janelidze@bitnet.ge>
2024-06-17 12:52:42 +0400

    quick fix

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { DashboardLayout } from "./dashboard-layout/DashboardLayout.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <DashboardLayout />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <DashboardLayout />,
+    },
+  ],
+  { basename: import.meta.env.VITE_BASE_URL || "/" }
+);
 
 export const Router = () => {
   return <RouterProvider router={router} />;
