0543b2f04694b5399ad631281a8fade32621be2f
Saba Janelidze <s.janelidze@bitnet.ge>
2024-06-17 13:14:45 +0400

    quick fix

--- src/pages/dashboard-layout/DashboardLayout.jsx
+++ src/pages/dashboard-layout/DashboardLayout.jsx
@@ -1,11 +1,13 @@
 import { Navigation } from "./components/Navigation";
 import { DashboardHeader } from "./components/DashboardHeader";
+import { Outlet } from "react-router-dom";
 
 export const DashboardLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <DashboardHeader />
       <Navigation />
+      <Outlet />
       {/* The rest of your dashboard content goes here */}
     </div>
   );
