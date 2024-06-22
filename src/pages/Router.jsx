71829c80352ebf61881a42c1ed59eb188b9e4108
Saba Janelidze <s.janelidze@bitnet.ge>
2024-06-22 23:27:47 +0400

    quick fix

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -7,37 +7,34 @@ import { Home } from "../pages/home/Home.jsx";
 import { ContactUs } from "./contact-us/ContactUs.jsx";
 import { ProductDetail } from "./product-details/ProductDetails.jsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/contact-us",
-          element: <ContactUs />,
-        },
-        {
-          path: "/product/:id",
-          element: <ProductDetail />,
-        },
-      ],
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-  ],
-  { basename: import.meta.env.VITE_BASE_URL || "/" }
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact-us",
+        element: <ContactUs />,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetail />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 export const Router = () => {
   return <RouterProvider router={router} />;
