fec2d73f70230a6b7fb03359688c31b0ce706381
Saba Janelidze <s.janelidze@bitnet.ge>
2024-06-22 22:56:13 +0400

    quick fix

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";  
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-export const Login = ({ registeredUsers }) => {  
-  const location = useLocation();
+export const Login = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
   const { t } = useTranslation();
-
-  const errorMessage = location.state ? location.state.error : "";
+  const { state } = useLocation();
+  const [error, setError] = useState("");
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -22,8 +20,9 @@ export const Login = ({ registeredUsers }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const user = registeredUsers.find(user => user.email === email && user.password === password);
-    if (user) {
+
+    if (email === state.email && password === state.password) {
+      console.log(email, state.email, password, state.password);
       navigate("/");
     } else {
       setError("Invalid email or password. Please try again.");
@@ -33,17 +32,22 @@ export const Login = ({ registeredUsers }) => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-beige">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h1 className="text-3xl font-bold mb-6 text-center text-brown">{t("login")}</h1>
-        {errorMessage && <p className="text-red-500 text-center mb-4">{errorMessage}</p>}
+        <h1 className="text-3xl font-bold mb-6 text-center text-brown">
+          {t("login")}
+        </h1>
         {error && <p className="text-red-500 text-center mb-4">{error}</p>}
         <form onSubmit={handleSubmit} className="mb-6">
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 font-semibold mb-2">
-            {t("email")}:
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              {t("email")}:
             </label>
             <input
               type="email"
               id="email"
+              autoComplete="email"
               value={email}
               onChange={handleEmailChange}
               className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -51,12 +55,16 @@ export const Login = ({ registeredUsers }) => {
             />
           </div>
           <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-700 font-semibold mb-2">
-            {t("password")}:
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              {t("password")}:
             </label>
             <input
               type="password"
               id="password"
+              autoComplete="current-password"
               value={password}
               onChange={handlePasswordChange}
               className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -64,13 +72,12 @@ export const Login = ({ registeredUsers }) => {
             />
           </div>
           <div className="flex items-center justify-between">
-            <a
-              href="/"
+            <button
+              type="submit"
               className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
             >
               {t("login")}
-            </a>
-            
+            </button>
           </div>
         </form>
       </div>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,53 +1,47 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";  
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { Img } from "/src/components";
 
 export const Register = () => {
   const [newEmail, setNewEmail] = useState("");
   const [newPassword, setNewPassword] = useState("");
-  const navigate = useNavigate();  
+  const navigate = useNavigate();
+
   const { t } = useTranslation();
+
   const handleEmailChange = (e) => {
     setNewEmail(e.target.value);
   };
-
 
   const handlePasswordChange = (e) => {
     setNewPassword(e.target.value);
   };
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
- 
-    console.log("Submitted:", newEmail, newPassword);
-   navigate("/login", { state: { email: newEmail, password: newPassword } });
-  };
 
-  
-  const handleFacebookLogin = () => {
-    window.FB.login(function(response) {
-      if (response.authResponse) {
-        console.log("Facebook login succeeded:", response);
-        
-      } else {
-        console.log("Facebook login failed:", response);
-      }
-    }, { scope: 'email' });
+    navigate("/login", { state: { email: newEmail, password: newPassword } });
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-beige ">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h1 className="text-3xl font-bold mb-6 text-center text-brown">{t("sign_up")}</h1>
+        <h1 className="text-3xl font-bold mb-6 text-center text-brown">
+          {t("sign_up")}
+        </h1>
         <form onSubmit={handleSubmit} className="mb-6">
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 font-semibold mb-2">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-semibold mb-2"
+            >
               {t("email")}:
             </label>
             <input
               type="email"
               id="email"
+              autoComplete="email"
               value={newEmail}
               onChange={handleEmailChange}
               className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -55,12 +49,16 @@ export const Register = () => {
             />
           </div>
           <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-700 font-semibold mb-2">
-            {t("password")}:
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              {t("password")}:
             </label>
             <input
               type="password"
               id="password"
+              autoComplete="current-password"
               value={newPassword}
               onChange={handlePasswordChange}
               className="w-full border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -76,16 +74,18 @@ export const Register = () => {
             </button>
             <button
               type="button"
-              onClick={handleFacebookLogin}
               className="bg-blue-700 text-white px-4 py-2 rounded-md flex items-center hover:bg-blue-800 transition duration-300"
             >
-              <img className="w-6 h-6 mr-2" src="/assets/images/fb.png" alt="Facebook logo" />
+              <Img className="w-6 h-6 mr-2" name="fb" alt="Facebook logo" />
               {t("Sign_fb")}
             </button>
           </div>
         </form>
         <p className="text-center text-gray-600">
-          {t("Already_have_acc")} <a href="/login" className="text-blue-600 hover:underline">{t("login")}</a>
+          {t("Already_have_acc")}{" "}
+          <a href="/login" className="text-blue-600 hover:underline">
+            {t("login")}
+          </a>
         </p>
       </div>
     </div>
