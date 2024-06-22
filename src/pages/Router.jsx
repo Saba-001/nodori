9eb1c33bd75999ee4c7705d3d5655f193e979bfa
Saba Janelidze <s.janelidze@bitnet.ge>
2024-06-22 23:21:02 +0400

    rem

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -6,33 +6,30 @@ import { Login } from "./login/Login.jsx";
 import { Home } from "../pages/home/Home.jsx";
 import { ContactUs } from "./contact-us/ContactUs.jsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <DashboardLayout />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/contact-us",
-          element: <ContactUs />,
-        },
-      ],
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-  ],
-  { basename: import.meta.env.VITE_BASE_URL || "/" }
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact-us",
+        element: <ContactUs />,
+      },
+    ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 export const Router = () => {
   return <RouterProvider router={router} />;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 export default defineConfig(() => ({
-  base: process.env.NODE_ENV === "development" ? "/" : "/nodori/",
   plugins: [react()],
 }));
