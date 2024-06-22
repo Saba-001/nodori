1088a7ac4fd84129d812f37addce5a6b834d88a7
Saba Janelidze <s.janelidze@bitnet.ge>
2024-06-22 14:19:55 +0400

    fix

--- src/components/Img.jsx
+++ src/components/Img.jsx
@@ -1,9 +1,9 @@
-export const Img = ({ className, name, src, ...restProps }) => {
+export const Img = ({ className, name, ...restProps }) => {
   const base = import.meta.env.VITE_BASE_URL || "";
 
   return (
     <img
-      src={src || base + "src/images/" + name + ".png"}
+      src={base + "/assets/images/" + name + ".png"}
       className={className}
       {...restProps}
     />
